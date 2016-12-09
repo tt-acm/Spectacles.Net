a0415b09e72a97164860e2899d16c2b64a21ccf9
William Wong <william.wong@wework.com>
2016-12-09 18:27:00 -0500

    Create Spectacles model classes and create base exporter class
    Extracted from the Spectacles.RevitExporter repository

---
+++ src/Spectacles.Net/Spectacles.Net/Data/Metadata.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+
+namespace Spectacles.Net.Data
+{
+  public class Metadata
+  {
+    [DataMember]
+    public string type { get; set; }      // "Object"
+
+    [DataMember]
+    public double version { get; set; }   // 4.3
+
+    [DataMember]
+    public string generator { get; set; } // "Spectacles.RevitExporter Revit Spectacles exporter"
+  }
+}
